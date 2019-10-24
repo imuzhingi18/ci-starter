5e4aba788f72ce283d9ba28ad718844225ce0147
sabs-r3 <itaimuzh@gmail.com>
2019-10-24 14:12:12 +0100

    Adding travis file Adding a doc-string

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -1,5 +1,7 @@
 import random
 class TemperatureSensor(object):
-
+    """"
+    A class that creates a temperature object.
+    """
     def check_temperature(self):
         return random.randint(-3, 38)
